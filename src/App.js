0d04f86ee9a6f33fa2f87c4799b3a541d83b0b40
Seong Su Park <sspark114@gmail.com>
2021-10-22 19:26:58 -0700

    Login with gmail to Firebase database

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LoginPage from './components/LoginPage';
 import LandingPage from './components/LandingPage';
 // import { Link } from 'react-router-dom';
 
+
 function App() {
-  // Temporary adminUser credentials until we get Firebase access
-  const adminUser = {
-    email: "admin@admin.com",
-    password: "admin123"
-  }
+
 
   const [user, setUser] = useState({name: "", email: ""});
   const [error, setError] = useState("");
 
-  const Login = details => {
-    console.log(details);
-  }
-
   const Logout = () => {
     console.log("Logout");
+    setUser({ name: "", email: ""});
   }
 
   return (
     <div className="App">
-      {(user.email != "") ?  (
-        <div className="welcome">
-          <h2>Welcome, <span>{user.name}</span></h2>
-          <button>Logout</button>
-        </div>
-      ) : (
-        <LoginPage />
-      )}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/landingpage" component={LandingPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
