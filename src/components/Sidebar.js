c15e80e4b3927eec662b9663ec73e95cf9aa9d84
Ritika Roy Choudhury <ritikarc11@gmail.com>
2021-10-25 13:58:31 -0700

    Styling added, but overlapping with current phonetaxx image set to background; need to remove that

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import { SidebarData } from './SidebarData'
+import './Sidebar.css'
 
 function Sidebar() {
     const [sidebar, setSidebar] = useState(false);
