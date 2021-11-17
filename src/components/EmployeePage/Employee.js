8f84ad2435c9e25c00b02bbd290179d458139bce
CoderMisha <52513965+CoderMisha@users.noreply.github.com>
2021-11-17 02:25:20 -0800

    Employee UI experiments
    Made skeleton for displaying employee data on page and played with UI. Card component will probably not work. Maybe Box?

--- src/components/EmployeePage/Employee.js
+++ src/components/EmployeePage/Employee.js
@@ -1,11 +1,68 @@
 import React from 'react';
- 
-const Employee = ({employee}) => {
-   return (
-       <div>
-            {employee.name} | {employee.role}
-        </div>
-   );
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+//Probably should not use card, maybe box?
+
+const Employee = ({ employee }) => {
+    return (
+        <Grid item xs={10}>
+            <Card maxWidth={345}>
+                <CardHeader
+                    title={employee.name}
+                    subheader={employee.role} />
+                <CardContent>
+                    <Button
+                        variant="contained"
+                        color="primary">View Profile
+                    </Button>
+                    <Typography component="div" variant="h5">
+                        ${employee.cost}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Grid>
+    );
 };
- 
+
 export default Employee;
+
+{/* <Grid item xs={10}>
+    <Card maxWidth={345}>
+        <CardHeader
+            title={employee.name}
+            subheader={employee.role} />
+        <CardContent>
+            <Button
+                variant="contained"
+                color="primary">View Profile
+            </Button>
+            <Typography component="div" variant="h5">
+                ${employee.cost}
+            </Typography>
+        </CardContent>
+    </Card>
+</Grid> */}
+
+
+{/* <Box
+    component="span"
+    m={1} //margin
+    border="1px solid black"
+    justifyContent="flex-end"
+    alignItems="flex-end"
+> {employee.name} | {employee.role}
+    <Button
+        variant="contained"
+        color="primary"
+        style={{ height: 30 }}
+    >
+        Employee Profile
+    </Button>
+</Box> */}
--- src/components/EmployeePage/EmployeeList.js
+++ src/components/EmployeePage/EmployeeList.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Employee from './Employee';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
  
 const EmployeeList = ({employeeList}) => {
    return (
-       <div>
-           {/* {employeeList.map(employee => {
-               return (
-                   <Employee employee={employee} />
-               )
-           })} */}
-       </div>
+       <Box padding={5}>
+            <Grid
+                container
+                direction="column"
+                //justifyContent="center"
+                alignItems="stretch"
+                columnSpacing={10}
+            >
+                {employeeList.map(employee => {
+                    return (
+                        <Employee employee={employee} />
+                    )
+                })}
+            </Grid>
+       </Box>
    );
 };
  
