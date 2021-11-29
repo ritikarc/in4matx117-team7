eb91a7b28b1ae75fd4dbb8478b671ce822e40732
Daniel Garcia <shmoogedebragah159@gmail.com>
2021-11-28 22:12:30 -0800

    Edited topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState, useContext } from 'react';
 import { UserContext } from '../../providers/UserProvider';
 import { auth } from '../../firebase';
 import firebase from '../../firebase';
+import mainLogo from '../../assets/PhoneTaxxIcon.png';
+import Avatar from 'react-avatar';
 
 function Topbar() {
   const user = useContext(UserContext);
@@ -10,7 +12,9 @@ function Topbar() {
 
   return (
     <div id="topbar">
+      <img src = {mainLogo} alt = "PhoneTaxx Logo" />
       <h2 className="admin-title">{name}</h2>
+      <Avatar name = {name} size = "50" /> 
       <h2>Email: {email}</h2>
       <button
         className="signout-btn"
